ad6cf09dd0f8c28a67f83347a1c8566628342c72
Mariel Garcia <maryel_565@hotmail.com>
2017-03-03 19:25:21 -0500

    validacion de formulario - input nombre

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 function validateForm(){
-	
+	var nombre= document.getElementById('name');
+	var apellido= document.getElementById('lastname');
+	var email= document.getElementById('input-email');
+	var pasword=document.getElementById('input-password')
+	var characterAllowed = /^[a-zA-Z]+$/;
+
+	if(nombre.value.length == 0 ){
+		alert("Ingresa tu Nombre");
+	}
+	else if(){
+		alert("Solo vamos a aceptar letras!");
+	}
+	else if(nombre.value.charAt(0) != nombre.value.charAt(0).toUpperCase()){
+		alert("Tu Nombre Debe comenzar con Mayúscula");
+	}
+	/*
+	if((nombre.value=="")||(expreRegular.test(nombre.value)==false)||(nombre.value.toString().charAt(0).toUpperCase()+nombre.value.toString().slice(1).toLowerCase()==false)){
+		alert("Ingrese correctamente su nombre");
+	};
+	if((apellido.value=="")||((expreRegular.test(apellido.value)==false)||(apellido.value.toString().charAt(0).toUpperCase()+apellido.value.toString().slice(1).toLowerCase()==false))){
+		alert("Ingrese correctamente su apellido");
+	};
+	if(email.value==""){
+		alert("Ingreso correctamente su apellido");
+	};
+	if((pasword.value=="")||(pasword.value.length>=7)||( pasword.value=="password")||(pasword.value=="123456")||(pasword.value=="098754")){
+		alert("Ingreso correctamente su contraseña");
+	}*/
 }
